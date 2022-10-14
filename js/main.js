2131d94884b48e35a59d9b843ae433c7176361f2
Krystal Furlani <krystal@Krystals-MacBook-Pro.local>
2022-10-14 10:13:22 -0400

    data not defined but data loading in otherwise

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ import { getData } from "./modules/dataMiner.js";
     console.log('hi!');
 
     let theThings = document.querySelector('#things-section'),
-        theTemplate = document.querySelector('#things-template').content; 
-        //faveData;
+        theTemplate = document.querySelector('#things-template').content, 
+        faveData;
         debugger;
 
    // getData();
@@ -15,7 +15,7 @@ import { getData } from "./modules/dataMiner.js";
     function buildThings(data) {
     
         debugger;
-        //faveData = data;
+        faveData = data;
 
         const things =Object.keys(data); 
 
@@ -26,8 +26,8 @@ import { getData } from "./modules/dataMiner.js";
             let containers = panel.firstElementChild.children;  
 
             containers[0].querySelector('img').src = `images/${data[thing].image}`;
-            //containers[0].querySelector('buttons').id = thing;
-            //containers[0].querySelector('buttons').addEventListener('click', showThing);
+            containers[0].querySelector('img').id = thing;
+            containers[0].querySelector('img').addEventListener('click', showThing);
 
             containers[1].textContent = data[thing].favouriteThing;
             containers[2].textContent = data[thing].description;
@@ -37,11 +37,11 @@ import { getData } from "./modules/dataMiner.js";
           })
     }
 
-    //function showThing(){
-     //   debugger;
+    function showThing(){
+        //debugger;
         
-       // let currentThing = faveData[this.id];
-    //}
+        let currentThing = faveData[this.id];
+    }
 
     getData('./data.json', buildThings);
 

68eebb04be5bdd165cd085861f6bd1005968157d
Krystal Furlani <krystal@Krystals-MacBook-Pro.local>
2022-10-14 01:21:03 -0400

    finally fetching data

--- js/main.js
+++ js/main.js
@@ -3,31 +3,31 @@
 import { getData } from "./modules/dataMiner.js";
 
 (() => {
-    console.log('fired!');
+    console.log('hi!');
 
-    let theThings = document.querySelector('.fave-lightbox'),
-        theTemplate = document.querySelector('#things-template').content, 
-        faveData;
+    let theThings = document.querySelector('#things-section'),
+        theTemplate = document.querySelector('#things-template').content; 
+        //faveData;
         debugger;
 
-    //getData();
+   // getData();
 
     function buildThings(data) {
     
         debugger;
-
-        faveData = data;
+        //faveData = data;
 
         const things =Object.keys(data); 
 
         things.forEach(thing => {
             
-            let panel = theTemplate.cloneNode(true);    
+            let panel = theTemplate.cloneNode(true);
+
             let containers = panel.firstElementChild.children;  
 
             containers[0].querySelector('img').src = `images/${data[thing].image}`;
-            //containers[0].querySelector('img').id = thing;
-            //containers[0].querySelector('button').addEventListener('click', showThing);
+            //containers[0].querySelector('buttons').id = thing;
+            //containers[0].querySelector('buttons').addEventListener('click', showThing);
 
             containers[1].textContent = data[thing].favouriteThing;
             containers[2].textContent = data[thing].description;
@@ -37,11 +37,11 @@ import { getData } from "./modules/dataMiner.js";
           })
     }
 
-    function showThing(){
-        debugger;
+    //function showThing(){
+     //   debugger;
         
-        let currentThing = faveData[this.id];
-    }
+       // let currentThing = faveData[this.id];
+    //}
 
     getData('./data.json', buildThings);
 
